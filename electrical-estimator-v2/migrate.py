2c3025d9006b316eb56041b104233bbf53d8688d
CarlosCruzValverde <carlosczvalverde@gmail.com>
2025-04-02 16:42:47 -0700

    Update migrate.py

--- electrical-estimator-v2/migrate.py
+++ electrical-estimator-v2/migrate.py
@@ -1,1 +1,9 @@
+from flask_migrate import Migrate, upgrade
+from your_app_module import create_app
 
+app = create_app()
+migrate = Migrate(app, app.db)
+
+if __name__ == '__main__':
+    with app.app_context():
+        upgrade()
